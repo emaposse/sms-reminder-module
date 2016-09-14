2819597a5be4baeb82b5bf15346886f7845bb7b6
nelson.mahumane <armando2>
2016-09-14 10:45:59 +0200

    contro da persistencia

---
+++ omod/src/main/java/org/openmrs/module/smsreminder/utils/GatewayStatusNotification.java
@@ -0,0 +1,7 @@
+package org.openmrs.module.smsreminder.utils;
+
+/**
+ * Created by nelson.mahumane on 23-08-2016.
+ */
+public class GatewayStatusNotification {
+}
---
+++ omod/src/main/java/org/openmrs/module/smsreminder/utils/SmsReminderHandler.java
@@ -0,0 +1,7 @@
+package org.openmrs.module.smsreminder.utils;
+
+/**
+ * Created by nelson.mahumane on 24-08-2016.
+ */
+public class SmsReminderHandler {
+}
